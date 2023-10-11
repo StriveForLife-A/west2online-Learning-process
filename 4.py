bde7698b231c76cf5f4ba2ccce4b8bb5a946cb26
StriveForLife-A <140537586+StriveForLife-A@users.noreply.github.com>
2023-10-11 17:26:52 +0800

    Update 4.py
    已修改

--- 4.py
+++ 4.py
@@ -1,6 +1,7 @@
-list=["west2online",12345678,"Python",12345,]
-newlist=[]
+list = ["west2online", 12345678, "Python", 12345, 234, 456]
+new_list = []
 for i in list:
-    if type(i)==int:
-        newlist.append(i)
-print(sorted(newlist))
+    if type(i) == int:
+        new_list.append(i)
+new_list.sort()
+print(new_list)
